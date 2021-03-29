f009af13a29bab06b80c8c50289f62dea8ce8f1a
Gianni GIUDICE <gianni.giudice@lacatholille.fr>
2021-03-29 16:04:19 +0200

    :lipstick: Amélioration affichage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
-
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-    // your config should go here
     apiKey: "AIzaSyCTOcgbLMMJzHXD6vRxlDh8tj1oU5u7cFY",
     authDomain: "gchat-1e0e9.firebaseapp.com",
     projectId: "gchat-1e0e9",
@@ -21,8 +18,7 @@ firebase.initializeApp({
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
-
-function App() {
+const App = () => {
     const [user] = useAuthState(auth);
 
     return (
@@ -33,44 +29,43 @@ function App() {
             </section>
         </div>
     );
-}
+};
 
-
-function SignIn() {
+const SignIn = () => {
     const signInWithGoogle = () => {
         const provider = new firebase.auth.GoogleAuthProvider();
         auth.signInWithPopup(provider);
     }
     return (
         <div className="flex flex-col h-screen items-center justify-center">
+            <div className="pt-5 text-center font-bold text-5xl text-white">GChat</div>
             <button className="mt-auto bg-white outline-none focus:outline-none p-2 m-3 border-b-4 border-gray-500 hover:bg-gray-200 hover:border-gray-500 transition-all duration-75 ease-in-out rounded inline-flex items-center" onClick={signInWithGoogle}>
                 <img className="w-5 mr-2 block" src={process.env.PUBLIC_URL + '/google-icon.svg'} alt='Google Icon' />
                 <span className="block">Sign In With Google</span>
             </button>
-            <p className="justify-self-end mt-auto text-white text-center text-opacity-50 hover:text-opacity-100">Made by <a className="hover:text-yellow-500" href="https://github.com/GianniGiudice">⚡Gianni GIUDICE</a>.</p>
+            <p className="justify-self-end mt-auto mb-5 text-white text-center text-opacity-50 hover:text-opacity-100">Made by <a className="hover:text-yellow-500" href="https://github.com/GianniGiudice">⚡Gianni GIUDICE</a>.</p>
         </div>
     )
-}
+};
 
-function SignOut() {
+const SignOut = () => {
     return auth.currentUser && (
         <div className="w-full">
             <button className="absolute hover:shadow-xl hover:text-blue-900 transition-all duration-75 ease-in-out rounded-full p-2 bg-white signout-btn material-icons" onClick={() => auth.signOut()}>exit_to_app</button>
         </div>
     )
-}
+};
 
-function ChatRoom() {
+const ChatRoom = () => {
     const dummy = useRef();
     const messagesRef = firestore.collection('messages');
     const query = messagesRef.orderBy('createdAt', 'asc').limitToLast(25);
-
     const [messages] = useCollectionData(query, {idField: 'id'});
     const [formValue, setFormValue] = useState('');
 
     const scrollToBottom = () => {
         dummy.current.scrollIntoView({ behavior: 'smooth' });
-    }
+    };
 
     useEffect(scrollToBottom, [messages]);
 
@@ -89,7 +84,7 @@ function ChatRoom() {
 
         setFormValue('');
         dummy.current.scrollIntoView({ behavior: 'smooth' });
-    }
+    };
 
     return (
         <div className="chat-bg w-full sm:w-2/3 p-2 rounded">
@@ -104,11 +99,10 @@ function ChatRoom() {
             </form>
         </div>
     )
-}
+};
 
-function ChatMessage(props) {
+const ChatMessage = (props) => {
     const { user, body, uid, photoURL, createdAt } = props.message;
-
     const messageClass = uid === auth.currentUser.uid ? 'flex-row-reverse' : 'flex-row';
     const messageBodyClass = uid === auth.currentUser.uid ? 'sent-message-bg text-right' : 'received-message-bg';
     const imageClass = uid === auth.currentUser.uid ? 'ml-2' : 'mr-2';
@@ -124,6 +118,6 @@ function ChatMessage(props) {
             </div>
         </div>
     )
-}
+};
 
 export default App;
